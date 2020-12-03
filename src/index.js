1848a443a632ee49eec7478cb11798be0d11c401
Eric M. Johnson <eric.johnson@housecallpro.com>
2020-12-02 20:31:14 -0800

    Add tests, fix tests,  and reorganize data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const days = require("./days");
 
 const fs = require('fs');
 
-let day01RawData = fs.readFileSync('./src/day01_data.json');
+let day01RawData = fs.readFileSync('./data/day01/part1.json');
 let day01Data = JSON.parse(day01RawData);
 console.log(`day01: ${days.day01(day01Data["entries"], day01Data["targetSum"])}`);
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,13 @@
 const days = require("./days");
 
-test("finds the two entries that sum to 2020 and multiplies them", () => {
-	expect(days.day01([1721, 979, 366, 299, 675, 1456], 2020)).toBe(514579);
-});
+describe("day01 part 1", () => {
+	test("finds the two entries that match a sum and multiplies them", () => {
+		expect(days.day01([1721, 979, 366, 299, 675, 1456], 2020)).toBe(514579);
+	})
+
+	test("throws an error when no two numbers sum correctly", () => {
+		expect(() => {
+			days.day01([1, 2, 3], 100);
+		}).toThrow('No two entries sum to 100');
+	})
+})
